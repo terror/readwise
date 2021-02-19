00ae90c6c1e0c7db72d494a0259dd0b0bc8e7ef3
Liam <liam@scalzulli.com>
2021-02-19 16:38:08 -0500

    Add main.rs example code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use readwise::*;
+
+extern crate dotenv;
+
+use dotenv::dotenv;
+use std::env;
+
 fn main() {
-  println!("Hello world!");
+  dotenv().ok();
+
+  let client = auth(&env::var("ACCESS_TOKEN").unwrap()).unwrap();
+
+  for book in client.books(1).unwrap() {
+    println!("{}\n", book.title);
+  }
 }
